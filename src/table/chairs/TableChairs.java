a92a89d58313080e5406d2ba4f910d6b508b36db
s73v3n18 <steveniege06@gmail.com>
2026-02-25 14:18:18 +0200

    FirstCommit T&C

---
+++ src/table/chairs/TableChairs.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package table.chairs;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TableChairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                 ***                  ");
+        System.out.println("              *   *   *               ");
+        System.out.println("                * * *                ");
+        System.out.println(" ||          ***********          || ");
+        System.out.println(" ||             * * *             || ");
+        System.out.println(" ||           *   *    *          || ");
+        System.out.println(" ||   (___)               (___)   || ");
+        System.out.println(" || ============================  || ");
+        System.out.println(" ||======||                ||=====|| ");
+        System.out.println(" ||      ||                ||     || ");
+        System.out.println(" ||      ||                ||     || ");
+        System.out.println(" ||      ||                ||     || ");
+        System.out.println(" ===================================");    
+        
+        
+    }
+    
+}
